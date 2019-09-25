bd592a074e71c52f1d0ca119f511dfbfa39035c1
Ivan Kirshak <i.kirshak@gmail.com>
2019-09-25 13:53:07 +0300

    another attempt to make to do list

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-let text = document.getElementById("text");
-let addBtn = document.getElementById("addBtn")
-let list = document.getElementById("list");
+// declare variables to find HTML elements
+
+let txtField = document.getElementById("text");
+let addBtn = document.getElementById("addBtn");
 let delBtn = document.getElementById("delBtn");
-let fullList = [];
+let list = document.getElementById("list");
 
-addBtn.addEventListener("click", function () {
-    let newLi = document.createElement('li'); 
-    newLi.className = "chosen"; 
-    newLi.innerText= text.value;
+// onclick Add Button will add a list item with text in txt-field
+addBtn.addEventListener("click", function() {
+    let listItem = document.createElement("li"); //new list item created
+    listItem.innerText = txtField.value; //created list item with text from txt-field
 
-    newLi.addEventListener("click", function(e) {
-        if(e.target.tagName = "li") {
-            e.target.style.backgroundColor = "yellow";
-            e.target.style.fontStyle = "italic";
-            
-            delBtn.addEventListener("click", function() {
-                if (confirm("Are you sure you want to remove an item?")) {
-                newLi.remove();
-                }
-            }, false)
-        }
+    //when some item is clicked, style of it changes
+    listItem.addEventListener("click", function() {
+        listItem.classList.add("chosen");
+        //remove marked item
+        delBtn.addEventListener("click", function() {
+            let toRemove = document.getElementsByClassName(".chosen");
+            toRemove.remove();
+        }, false)
     }, false)
 
-    list.appendChild(newLi);
-    fullList.push(text.value);
+    // remove styles and unmark the item
+    listItem.addEventListener("dblclick", function() {
+        listItem.classList.remove("chosen");
+    }, false)
+
+    // this helps a new list item to be created and shown on page:
+    list.appendChild(listItem); 
 }, false)
